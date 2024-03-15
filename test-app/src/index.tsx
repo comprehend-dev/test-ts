0ebd7eaad5342b809519b298c279f885eac27569
Altai-man <alexander.kiryuhin@gmail.com>
2024-03-15 18:43:35 +0100

    Convert index.js to index.tsx
    Plus add a class and a function as negative test cases (containing no XML).

--- test-app/src/index.tsx
+++ test-app/src/index.tsx
@@ -11,6 +11,14 @@ root.render(
   </React.StrictMode>
 );
 
+class NonReactClassExample {
+
+}
+
+function nonReactFunction(): number {
+    return 42;
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
