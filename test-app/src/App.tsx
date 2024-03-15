bef924e83df602e2357a4ceea0b4566e67aafdcd
Altai-man <alexander.kiryuhin@gmail.com>
2024-03-15 18:37:55 +0100

    Convert the test app to TypeScript

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class TestReactClassComponent extends React.Component {
@@ -12,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
